1771399da01aa343a2bbd544c7ca1a217e3beef6
Santosh Tatikonda <santosh.tatikonda@outlook.com>
2021-04-17 12:04:18 -0700

    Adding constryctor.py file

--- constructor.py
+++ constructor.py
@@ -1,8 +1,14 @@
 class Calculator:
     num = 100
 
-    def __init__(self):
-        print("Constructor called when object is created")
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def sum(self, a, b):
+        print(self.a + self.b)
 
 
-obj = Calculator()
+obj = Calculator(1, 2)
+obj.sum(2, 3)
+
